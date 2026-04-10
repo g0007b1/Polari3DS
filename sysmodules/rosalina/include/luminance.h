f21b4fa7adc7e0c2e0236f0e720cf74cfeb7d44b
g0007b1 <73489208+g0007b1@users.noreply.github.com>
2026-04-10 18:48:05 +0300

    Update luminance.h
    Signed-off-by: g0007b1 <73489208+g0007b1@users.noreply.github.com>

--- sysmodules/rosalina/include/luminance.h
+++ sysmodules/rosalina/include/luminance.h
@@ -32,7 +32,7 @@
 
 /* Raw luminance cap (Rosalina "Change screen brightness" + recalibration). Match stock Polari extended max if unset. */
 #ifndef POLARI_ROSALINA_BRIGHTNESS_TRUE_MAX
-#define POLARI_ROSALINA_BRIGHTNESS_TRUE_MAX 350
+#define POLARI_ROSALINA_BRIGHTNESS_TRUE_MAX 500
 #endif
 
 u32 getMinLuminancePreset(bool top);
